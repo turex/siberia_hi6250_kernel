536ac5b1b789180f5597d5ca8cbb3ad2cb792e59
Wolfram Sang <wsa+renesas@sang-engineering.com>
2016-01-29 09:27:50 +0100

    mmc: sanitize 'bus width' in debug output
    commit ed9feec72fc1fa194ebfdb79e14561b35decce63 upstream.
    
    The bus width is sometimes the actual bus width, and sometimes indices
    to different arrays encoding the bus width. In my debugging case "2"
    could mean 8-bit as well as 4-bit, which was extremly confusing. Let's
    use the human-readable actual bus width in all places.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1322,7 +1322,7 @@ void mmc_set_ios(struct mmc_host *host)
 		"width %u timing %u\n",
 		 mmc_hostname(host), ios->clock, ios->bus_mode,
 		 ios->power_mode, ios->chip_select, ios->vdd,
-		 ios->bus_width, ios->timing);
+		 1 << ios->bus_width, ios->timing);
 
 	host->ops->set_ios(host, ios);
 #ifdef CONFIG_HUAWEI_EMMC_DSM
