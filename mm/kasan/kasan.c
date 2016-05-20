015ca4c8a6d1913d92aee5bfa9f10010cabc4a8c
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-05-20 16:59:20 -0700

    mm/kasan: print name of mem[set,cpy,move]() caller in report
    commit 936bb4bbbb832f81055328b84e5afe1fc7246a8d upstream.
    
    When bogus memory access happens in mem[set,cpy,move]() it's usually
    caller's fault.  So don't blame mem[set,cpy,move]() in bug report, blame
    the caller instead.
    
    Before:
      BUG: KASAN: out-of-bounds access in memset+0x23/0x40 at <address>
    After:
      BUG: KASAN: out-of-bounds access in <memset_caller> at <address>
    
    Link: http://lkml.kernel.org/r/1462538722-1574-2-git-send-email-aryabinin@virtuozzo.com
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Acked-by: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/kasan/kasan.c
+++ mm/kasan/kasan.c
@@ -252,7 +252,6 @@ static __always_inline bool memory_is_poisoned(unsigned long addr, size_t size)
 	return memory_is_poisoned_n(addr, size);
 }
 
-
 static __always_inline void check_memory_region_inline(unsigned long addr,
 						size_t size, bool write,
 						unsigned long ret_ip)
