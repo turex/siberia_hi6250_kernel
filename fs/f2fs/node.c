5b2f684ac4b5b3f81cd8c3def16e393c5e77a199
Chao Yu <yuchao0@huawei.com>
2016-12-13 18:54:59 +0800

    f2fs: don't cache nat entry if out of memory
    If we run out of memory, in cache_nat_entry, it's better to avoid loop
    for allocating memory to cache nat entry, so in low memory scenario, for
    read path of node block, I expect this can avoid unneeded latency.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -280,8 +280,9 @@ static void cache_nat_entry(struct f2fs_sb_info *sbi, nid_t nid,
 
 	e = __lookup_nat_cache(nm_i, nid);
 	if (!e) {
-		e = grab_nat_entry(nm_i, nid);
-		node_info_from_raw_nat(&e->ni, ne);
+		e = grab_nat_entry(nm_i, nid, false);
+		if (e)
+			node_info_from_raw_nat(&e->ni, ne);
 	} else {
 		f2fs_bug_on(sbi, nat_get_ino(e) != le32_to_cpu(ne->ino) ||
 				nat_get_blkaddr(e) !=
